61470184e61140573ff5db14706ed8483583c893
David <juanlc1526@gmail.com>
2024-12-23 21:52:50 -0500

    final review

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,35 @@
 const Footer = () => {
   return (
-    <div className="w-full h-[30vh] max-sm:h-[40vh] flex max-sm:flex-col items-center justify-around font-poppins bg-blanco" id="contacto">
-      <div className="flex flex-col items-center justify-center">
+    <div
+      className="w-full bg-blanco font-poppins p-6"
+      id="contacto"
+    >
+      <div className="flex flex-col items-center text-center max-w-screen-lg mx-auto">
         <img
-        className="w-40"
+          className="w-32 mb-4"
           src="assets/logo.webp"
           alt="academia moderna de automovilismo cuba"
-        ></img>
-        <br></br>
-        <h2 className="text-black">Academia moderna de automovilismo cuba, Pereira</h2>
-      </div>
-      <div>
+        />
+        <h2 className="text-black text-sm sm:text-base leading-relaxed max-w-md mb-4">
+          Tu mejor opción en Pereira, ubicada estratégicamente cerca del sector
+          de Cuba. Ofrecemos cursos completos para obtener tu licencia de
+          conducir a precios accesibles y promociones especiales en paquetes
+          combinados. Aprende a manejar con facilidad y aprovecha nuestras
+          ofertas exclusivas. ¡Estamos cerca de ti para ayudarte a comenzar tu
+          camino al volante!
+        </h2>
         <a
           href="https://api.whatsapp.com/send?l=es&phone=573113187351"
           aria-label="Whatsapp academia moderna de automovilismo cuba"
           target="_blank"
           rel="noopener noreferrer"
-          className="mr-2"
         >
           <img
             src="icons/whatsapp-icon.svg"
             alt="Whatsapp academia moderna de automovilismo cuba"
-          ></img>
+            className="w-8"
+          />
         </a>
-       
-
-        <a href="https://davidladino.netlify.app/"
-          aria-label="David Ladino Portfolio"
-          target="_blank"
-          className="text-black text-center absolute mt-10"
-          rel="noopener noreferrer">Developed by David Ladino</a>
       </div>
     </div>
   );
